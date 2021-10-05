529c4607656f43aec3923b85369b9d418e41f842
Jinwoo-Home <back3946@gmail.com>
2021-10-05 20:59:07 +0900

    Test code 까지 작성

--- src/test/java/hello/hellospring/repository/MemoryMemberRepositoryTest.java
+++ src/test/java/hello/hellospring/repository/MemoryMemberRepositoryTest.java
@@ -1,11 +1,22 @@
 package hello.hellospring.repository;
 
 import hello.hellospring.domain.Member;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class MemoryMemberRepositoryTest {
 
     MemoryMemberRepository repository = new MemoryMemberRepository();
+
+    // 각 메소드가 끝난 뒤에 저장공간 클리어
+    @AfterEach
+    public void afterEach() {
+        repository.clearStore();
+    }
 
     @Test
     public void save() {
@@ -14,6 +25,41 @@ public class MemoryMemberRepositoryTest {
 
         repository.save(member);
 
+        Member result = repository.findById(member.getId()).get();
 
+//        System.out.println("result = " + (result == member));
+        assertThat(member).isEqualTo(result);
+//        assertThat(member).isEqualTo(null);
+    }
+
+    @Test
+    public void findByName() {
+        Member member1 = new Member();
+        member1.setName("spring1");
+        repository.save(member1);
+
+        Member member2 = new Member();
+        member2.setName("spring2");
+        repository.save(member2);
+
+        Member result = repository.findByName("spring1").get(); // get()으로 받아오면 Optional 을 깔수있다.
+
+        assertThat(result).isEqualTo(member1);
+    }
+
+    @Test
+    public void findAll() {
+        Member member1 = new Member();
+        member1.setName("spring1");
+        repository.save(member1);
+
+        Member member2 = new Member();
+        member2.setName("spring2");
+        repository.save(member2);
+
+        List<Member> result = repository.findAll();
+
+        // Assertions 을 static import 해서 그냥 사용
+        assertThat(result.size()).isEqualTo(2);
     }
 }
