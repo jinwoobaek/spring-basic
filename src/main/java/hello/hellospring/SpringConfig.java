94bd107cc6ed684f06d6243114720141430f164c
jinwoo_baek <back3946@gmail.com>
2021-10-08 15:17:52 +0900

    Bean 주입 방식 Component Scan -> JavaCode 설정(Spring Config) 방식으로 변경

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -3,7 +3,6 @@ package hello.hellospring;
 import hello.hellospring.repository.JdbcMemberRepository;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.service.MemberService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
